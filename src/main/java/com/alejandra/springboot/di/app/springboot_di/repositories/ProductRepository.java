1492430cbd5aa95e31097f1246f2ea38dc1afd98
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-26 20:13:50 +0200

    Implementando metodo findById en la clase Repository

--- src/main/java/com/alejandra/springboot/di/app/springboot_di/repositories/ProductRepository.java
+++ src/main/java/com/alejandra/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -21,5 +21,9 @@ public class ProductRepository {
     public List<Product> findAll() {
         return data;
     }
+
+    public Product findById(Long id) {
+        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElseThrow();
+    }
     
 }
