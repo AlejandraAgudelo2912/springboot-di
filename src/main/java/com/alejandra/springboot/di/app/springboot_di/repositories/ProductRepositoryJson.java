fb851e246311e94b39506735009fa50ed7aadf72
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-27 16:20:32 +0200

    Crando clase que lee los datos de un archivo json

---
+++ src/main/java/com/alejandra/springboot/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,42 @@
+package com.alejandra.springboot.di.app.springboot_di.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.alejandra.springboot.di.app.springboot_di.models.Product;
+
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.ObjectMapper;
+
+public class ProductRepositoryJson implements ProductRepository{
+
+    private List<Product> list;
+    
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (JacksonException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+}
