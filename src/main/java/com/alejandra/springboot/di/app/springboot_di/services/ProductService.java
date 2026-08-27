322908a1c248e9c56a0e7c5ce06b58d67b735c15
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-27 12:34:33 +0200

    Ecribiendo las interfaces Repository y Service

--- src/main/java/com/alejandra/springboot/di/app/springboot_di/services/ProductService.java
+++ src/main/java/com/alejandra/springboot/di/app/springboot_di/services/ProductService.java
@@ -1,26 +1,13 @@
 package com.alejandra.springboot.di.app.springboot_di.services;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.alejandra.springboot.di.app.springboot_di.models.Product;
-import com.alejandra.springboot.di.app.springboot_di.repositories.ProductRepository;
 
-public class ProductService {
+public interface ProductService {
 
-    private ProductRepository repository = new ProductRepository();
+    List<Product> findAll();
+    
+    Product findById(Long id);
 
-    public List<Product> findAll(){
-        return repository.findAll().stream().map(p -> {
-            Double priceTax = p.getPrice() * 1.25d;
-            // Product newProd = new Product(p.getId(),p.getName(),priceImp.longValue());
-            Product newProd = (Product) p.clone();
-            newProd.setPrice(priceTax.longValue());
-            return newProd;
-        }).collect(Collectors.toList());
-    }
-
-    public Product findById(Long id){
-        return repository.findById(id);
-    }
 }
---
+++ src/main/java/com/alejandra/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -0,0 +1,30 @@
+package com.alejandra.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.alejandra.springboot.di.app.springboot_di.models.Product;
+import com.alejandra.springboot.di.app.springboot_di.repositories.ProductRepositoryImpl;
+
+public class ProductServiceImpl implements ProductService{
+
+    private ProductRepositoryImpl repository = new ProductRepositoryImpl();
+
+    @Override
+    public List<Product> findAll(){
+        return repository.findAll().stream().map(p -> {
+            Double priceTax = p.getPrice() * 1.25d;
+            // Product newProd = new Product(p.getId(),p.getName(),priceImp.longValue());
+            Product newProd = (Product) p.clone();
+            newProd.setPrice(priceTax.longValue());
+            return newProd;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public Product findById(Long id){
+        return repository.findById(id);
+    }
+
+
+}
