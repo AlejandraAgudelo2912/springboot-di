a7c4d204530b62f3611949844b4c522d457f727c
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-27 16:00:26 +0200

    El contexto SessionScope

--- src/main/java/com/alejandra/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/alejandra/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.context.annotation.RequestScope;
 
 import com.alejandra.springboot.di.app.springboot_di.models.Product;
 
 @Primary
-@RequestScope
+//@RequestScope
+//@SessionScope
 @Repository("productList")
 public class ProductRepositoryImpl implements ProductRepository{
 
